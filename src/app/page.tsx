6255a7fb4b3557a170835e25ca0d2a7a9ab58e2f
Yug Gupta <70101980+yugg98@users.noreply.github.com>
2024-07-03 23:42:17 +0530

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
             className=" md:block hidden mx-auto  w-screen"
             autoPlay
             loop
+            muted
             // controls
           ></video>
             <video
